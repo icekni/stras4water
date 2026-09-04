ce88835d9a0e536e42f6c8c4192179c477a43f02
Cédric JOSSO <Cdric JOSSO@LAPTOP-GV4XP73>
2026-09-04 14:07:56 +0200

    dev du live

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -6,47 +6,64 @@ use App\Service\CoverService;
 use App\Service\DisplayStateProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DisplayController extends AbstractController
 {
-    #[Route('/live', name:'display_live')]
+    #[Route('/live', name: 'display_live')]
     public function live(
         DisplayStateProvider $provider
-    ): Response
-    {
+    ): Response {
         return $this->render(
             'display/live.html.twig',
             [
-
                 'state' => $provider->getState()
-
             ]
         );
     }
 
-    #[Route('/api/display/state', name:'display_state')]
+    #[Route(
+        '/api/display/state',
+        name: 'display_state',
+        methods: ['GET']
+    )]
     public function state(
         DisplayStateProvider $provider
-    ): JsonResponse
-    {
+    ): JsonResponse {
         return $this->json(
             $provider->getState()
         );
     }
 
-    #[Route('/api/display/test-cover', name: 'display_test_cover')]
-    public function testCover(
-        CoverService $coverService
+    #[Route(
+        '/api/display/state',
+        name: 'display_state_update',
+        methods: ['POST']
+    )]
+    public function updateState(
+        Request $request,
+        DisplayStateProvider $provider
     ): JsonResponse {
-        $cover = $coverService->getCover(
-            'Marc Anthony',
-            'Vivir Mi Vida (Official Remix Version Extended Dance Floor)'
+        $data = json_decode(
+            $request->getContent(),
+            true
         );
 
+        if (!is_array($data)) {
+            return $this->json(
+                [
+                    'error' => 'JSON invalide'
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $provider->updateState($data);
+
         return $this->json([
-            'cover' => $cover,
+            'success' => true
         ]);
     }
 }
