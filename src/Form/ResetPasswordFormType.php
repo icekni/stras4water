2247c94d70a2427fb192c9e48b52efa070636a71
Josso Cédric <cjosso@gmail.com>
2026-09-21 18:44:13 +0200

    mot de passe oublié

---
+++ src/Form/ResetPasswordFormType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResetPasswordFormType extends AbstractType
+{
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Modifier mon mot de passe',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => true,
+        ]);
+    }
+}
